245a4c3d041609ebf25861ba6c75d4cd7dddc543
danielcefasmitma <cefasdaniel87@gmail.com>
2026-06-10 15:39:58 -0400

    add: habilitacion de variables de entorno

---
+++ docs/comandos.md
@@ -0,0 +1,18 @@
+## Comandos necesarios
+
+- Instalar nest globalmente
+```
+pnpm add @nestjs/cli
+```
+- crear un proyecto 
+```
+nest new nombre-proyecto
+```
+- Instalar Swagger
+```
+pnpm add @nestjs/swagger
+```
+- Instalar config para las variables de entorno
+```
+pnpm add @nestjs/config
+```
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('CRUD Inventarios')
+    .setDescription('Crud nest-inventarios-cv')
+    .setVersion('1.0')
+    .addTag('inventarios')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+  // end swagger
   await app.listen(process.env.PORT ?? 3000);
-}
+} 
 bootstrap();
